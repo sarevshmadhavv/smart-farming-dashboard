9c30169398cb480bf3d1d727f90767b2558a8b17
Madhav <229004092@sastra.ac.in>
2025-10-08 22:39:04 +0530

    Initial commit - AI Smart Farming Dashboard

--- farm_dashboard.py
+++ farm_dashboard.py
@@ -67,7 +67,7 @@ if not st.session_state["logged_in"]:
     st.title("🌱 AI Smart Farming Dashboard - Login / Register")
     tab1, tab2 = st.tabs(["🔑 Login", "📝 Register"])
 
-    background_url = "https://plus.unsplash.com/premium_photo-1661907005604-cec7ffb6a042?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    background_url = "https://pin.it/622xl3x8O"
     st.markdown(
         f"""
         <style>
